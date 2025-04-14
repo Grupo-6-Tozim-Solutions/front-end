518697f6183be0a866c3a39960dbb2c455dfe9ee
Brenno Santos Alves <160975412+brennosantoss@users.noreply.github.com>
2025-04-14 13:23:59 -0300

    ajustes finais

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import LoginPage from './pages/LoginPage';
 import CadastroPage from './pages/CadastroPage';
 
+
 // import './App.css';
 
 function App() {
   return (
     <div >
    <CadastroPage/>
-   <LoginPage/>
+   {/* <LoginPage/> */}
     </div>
   );
 }
